bbd50262d4b0324046a98f3ff27875b35fd70ecc
ladanziaee@gmail.com <ladanziaee@gmail.com>
2018-02-22 20:57:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ return string.toLowerCase("hello")
 function sayHiToGrandma(string)
 {
   return console.log("I cant hear you!")
-  return console.log("I love you, too")
+console.log("I love you, too")
 }
 
 function logShout(string)
